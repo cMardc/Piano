cc68d4d984dcffff289cb23664bd5105c3b3a06c
cMardc <merd.yedek@gmail.com>
2023-04-24 14:46:17 +0400

    Add project files.

---
+++ Piano/Piano.cpp
@@ -0,0 +1,45 @@
+﻿#include "Piano.h"
+using namespace std;
+
+#pragma comment(lib, "Winmm.lib")
+
+int main(int argc, char* argv[])
+{
+	while (true)
+	{
+		if (_kbhit())
+		{
+			int press = getch();
+			char btn = press;
+			btn = tolower(btn);
+			if (btn == 'a')
+			{
+				PlaySound("D:\\Piano\\Piano\\Piano Keys\\keyA.wav", NULL, SND_FILENAME);
+			}
+			else if (btn == 'b')
+			{
+				PlaySound("D:\\Piano\\Piano\\Piano Keys\\keyB.wav", NULL, SND_FILENAME);
+			}
+			else if (btn == 'c')
+			{
+				PlaySound("D:\\Piano\\Piano\\Piano Keys\\keyC.wav", NULL, SND_FILENAME);
+			}
+			else if (btn == 'd')
+			{
+				PlaySound("D:\\Piano\\Piano\\Piano Keys\\keyD.wav", NULL, SND_FILENAME);
+			}
+			else if (btn == 'e')
+			{
+				PlaySound("D:\\Piano\\Piano\\Piano Keys\\keyE.wav", NULL, SND_FILENAME);
+			}
+			else if (btn == 'f')
+			{
+				PlaySound("D:\\Piano\\Piano\\Piano Keys\\keyF.wav", NULL, SND_FILENAME);
+			}
+			else if (btn == 'g')
+			{
+				PlaySound("D:\\Piano\\Piano\\Piano Keys\\keyG.wav", NULL, SND_FILENAME);
+			}
+		}
+	}
+}
---
+++ Piano/Piano.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+//Libraries Used
+#include <iostream> //Input/Output Stream For Console
+#include <string>   //For String
+#include <fstream>  //File Stream 
+#include <vector>   //Changable Index Structure
+#include <Windows.h>//For System Commands
+#include <algorithm>//Easy Sorting For Arrays/Vectors ...
+#include <iomanip>  //For Outputting With Given Precision
+#include <cmath>    //Easy Math Functions
+#include <stdio.h>  //Standart Input/Output For Given Types
+#include <conio.h>  //For Getting Key Presses
+#include <ctime>    //For Gettig Real Time
+#include <filesystem>
+#include <mmsystem.h>
+#include <Mmsystem.h>
+#include <mciapi.h>
+#pragma comment(lib, "Winmm.lib")
+
+using namespace std;//Standart Namespace
